788515a956a7c7c867b8062af6ad620b65e203f7
Matias Cusinato <matimcusi@gmail.com>
2020-04-21 17:26:12 -0300

    se modificaron los archivos

---
+++ resources/views/prueba.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<div>
+		<h3> Nombre del instituto : Sedes Sapientiae </h3>
+
+        <h3> Materia : Programacion III </h3>
+
+        <h3> Año : 3er año </h3>
+
+        <h3> Nombre y apellido : Matias Cusinato </h3>
+    </div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/url_prueba', function () {
+	return ('buenas');
+});
+
+route::get('/prueba', function () {
+	return view('prueba');
+});
